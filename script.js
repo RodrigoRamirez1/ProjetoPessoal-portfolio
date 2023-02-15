e58a6540198f3e80f2741e88fda6841e112bfeab
Rodrigo Ramirez <rodrigopinheiroramirez@gmail.com>
2023-02-15 16:28:33 -0300

    Ajustando a condicional

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function carregar() {
   var date = new Date()
   var hora = date.getHours()
 
-  if (hora >= 5 && hora < 19) {
+  if (hora >= 9 && hora < 18) {
     const html = document.documentElement
     html.classList.toggle("light")
   } else {
