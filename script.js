7278d70f1105fac1b425cdfa8dfed68ad05d08c6
Rodrigo Ramirez <rodrigopinheiroramirez@gmail.com>
2023-02-23 10:50:49 -0300

    Add outro backgrond no body do projeto e desativando a função JS

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function carregar() {
-  var date = new Date()
-  var hora = date.getHours()
+// function carregar() {
+//   var date = new Date()
+//   var hora = date.getHours()
 
-  if (hora >= 9 && hora < 18) {
-    const html = document.documentElement
-    html.classList.toggle("light")
-  } else {
-    html.classList.toggle("root")
-  }
-}
+//   if (hora >= 9 && hora < 18) {
+//     const html = document.documentElement
+//     html.classList.toggle("light")
+//   } else {
+//     html.classList.toggle("root")
+//   }
+// }
